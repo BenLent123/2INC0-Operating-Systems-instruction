7ea731a462ef6d0c5985311c4542791f6a1da771
Ben Lentschig <134686333+BenLent123@users.noreply.github.com>
2024-11-15 13:58:52 +0100

    Update 13112024Instruction1.c

--- instruction1/13112024Instruction1.c
+++ instruction1/13112024Instruction1.c
@@ -5,6 +5,12 @@
 int main(int argc, char * argv[])
 {
 printf("Hello world\n");
-
-
+int a = 20;
+int b = 33;
+int array[30];
+char chararray[30];
+p* = &a;
+int* ptr;
+int n = 5;
+ptr = (int*)malloc(n * sizeof(int));
 }
